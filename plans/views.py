bb697889ddc1469e8b456a211e5aa596fc0ceeb1
Junyoung Yang <tom9744@gmail.com>
2020-09-05 17:05:04 +0900

    TodoList Ordering & Searching

--- plans/views.py
+++ plans/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Tag, Plan, BucketList
@@ -61,9 +62,13 @@ class PlanViewSet(viewsets.ModelViewSet):
 class BucketListViewSet(viewsets.ModelViewSet):
     """ 데이터베이스의 하고싶은 일 모델을 관리한다. """
     serializer_class = BucketListSerializer
-    queryset = BucketList.objects.all().order_by('-id')
+    queryset = BucketList.objects.all()
     authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated, ]
+    filter_backends = [SearchFilter, OrderingFilter, ]
+
+    search_fields = ['is_complete']
+    ordering = ['-id']
 
     def perform_create(self, serializer):
         """ 새로운 일정을 생성하고, 현재 사용중인 사용자를 연결한다. """
