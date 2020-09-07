432537e6e2774ffeb7a394c54c3f01bb0f8e4343
Junyoung Yang <tom9744@gmail.com>
2020-09-07 23:07:53 +0900

    Token Life Cycle Change

--- planner/settings.py
+++ planner/settings.py
@@ -147,8 +147,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'USER_ID_CLAIM': 'id',
 }
 
